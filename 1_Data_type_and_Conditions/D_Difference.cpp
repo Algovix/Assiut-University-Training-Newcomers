6ff553f3bb537f5124575f84ea403be06b9f82e5
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 02:56:52 +0300

    Optimize input/output performance in D_Difference.cpp

--- 1_Data_type_and_Conditions/D_Difference.cpp
+++ 1_Data_type_and_Conditions/D_Difference.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     long long A, B, C, D;
     cin >> A >> B >> C >> D;
     cout << "Difference" << " = " << (A * B) - (C * D);
