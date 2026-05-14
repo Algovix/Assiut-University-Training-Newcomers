564fc035e9a3000639a8eb3c02a0d855f80a9747
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 02:56:33 +0300

    Update C_Simple_Calculator.cpp

--- 1_Data_type_and_Conditions/C_Simple_Calculator.cpp
+++ 1_Data_type_and_Conditions/C_Simple_Calculator.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     long long x, y;
     cin >> x >> y;
     
