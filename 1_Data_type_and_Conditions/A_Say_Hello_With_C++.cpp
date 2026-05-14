eaeeb48994334a1a58d43f6c53115604d178349d
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 01:12:54 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/A_Say_Hello_With_C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+int main() {
+    string S;
+    cin >> S;
+    cout << "Hello, " << S << "\n";
+
+    return 0;
+}
