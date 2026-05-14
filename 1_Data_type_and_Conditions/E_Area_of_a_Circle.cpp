bc90d9951bcbebccb1eb848decf75157fde06094
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 02:57:20 +0300

    Update E_Area_of_a_Circle.cpp

--- 1_Data_type_and_Conditions/E_Area_of_a_Circle.cpp
+++ 1_Data_type_and_Conditions/E_Area_of_a_Circle.cpp
@@ -4,8 +4,9 @@
 // https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
 
 using namespace std;
-int main()
-{
+int main() {
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
      cout << fixed << setprecision(9);
      float R;
      cin >> R;
