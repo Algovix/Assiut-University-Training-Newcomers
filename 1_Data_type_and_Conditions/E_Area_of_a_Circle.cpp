ed3394dc906e43caeea4f3ab8e6e79e40986d1ec
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 02:12:26 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/E_Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+using namespace std;
+int main()
+{
+     cout << fixed << setprecision(9);
+     float R;
+     cin >> R;
+     cout << 3.141592653 * R * R;
+     return 0;
+}
