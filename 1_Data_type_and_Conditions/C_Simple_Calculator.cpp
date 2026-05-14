e369551c245d898e7774b7b571fabd74d56ebc3a
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 01:58:49 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/C_Simple_Calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+int main() {
+    long long x, y;
+    cin >> x >> y;
+    
+    cout << x << " + " << y << " = " << x + y << "\n";
+    cout << x << " * " << y << " = " << x * y << "\n";
+    cout << x << " - " << y << " = " << x - y << "\n";
+
+    return 0;
+}
