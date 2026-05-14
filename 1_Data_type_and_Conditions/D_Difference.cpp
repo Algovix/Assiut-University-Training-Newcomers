79a9918083970a2003954d1f6d9b94c67cb6509d
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 01:59:47 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/D_Difference.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+int main() {
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << "Difference" << " = " << (A * B) - (C * D);
+    return 0;
+}
