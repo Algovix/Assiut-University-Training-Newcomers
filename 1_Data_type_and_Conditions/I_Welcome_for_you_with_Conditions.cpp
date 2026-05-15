e5039618e70a7db3833e0506e8c7f889885bab9d
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 14:58:54 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+using namespace std;
+int main(){
+    short A, B;
+    cin >> A >> B;
+    cout << (A >= B ? "Yes" : "No");
+    return 0;
+}
