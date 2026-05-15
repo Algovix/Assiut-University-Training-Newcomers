2bac68349d4a47a88a225e126a2a4d3798518c61
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 20:31:55 +0300

    Update A_Say_Hello_With_C++.cpp

--- 1_Data_type_and_Conditions/A_Say_Hello_With_C++.cpp
+++ 1_Data_type_and_Conditions/A_Say_Hello_With_C++.cpp
@@ -4,8 +4,6 @@ using namespace std;
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     string S;
     cin >> S;
     cout << "Hello, " << S << "\n";
