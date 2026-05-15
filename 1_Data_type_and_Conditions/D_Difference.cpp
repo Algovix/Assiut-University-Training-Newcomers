d9b5b506f503c643413c25272eebd93eabc3ccd9
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 20:32:32 +0300

    Update D_Difference.cpp

--- 1_Data_type_and_Conditions/D_Difference.cpp
+++ 1_Data_type_and_Conditions/D_Difference.cpp
@@ -4,8 +4,6 @@ using namespace std;
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     long long A, B, C, D;
     cin >> A >> B >> C >> D;
     cout << "Difference" << " = " << (A * B) - (C * D);
