9ee33ec6fa55667e4b45c74650b6bebd3a6cdd22
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-15 18:32:31 +0300

    Add files via upload

---
+++ 1_Data_type_and_Conditions/Q_Coordinates_of_a_Point.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Q
+
+using namespace std;
+int main() {
+    double X, Y;
+    cin >> X >> Y;
+    if (X == 0 && Y == 0) {
+        cout << "Origem\n";
+    } else if (Y == 0) {
+        cout << "Eixo X\n";
+    } else if (X == 0) {
+        cout << "Eixo Y\n";
+    } else if (X > 0 && Y > 0) {
+        cout << "Q1\n";
+    } else if (X < 0 && Y > 0) { 
+        cout << "Q2\n";
+    } else if (X < 0 && Y < 0) { 
+        cout << "Q3\n";
+    } else { 
+        cout << "Q4\n";
+    }
+
+    return 0;
+}
