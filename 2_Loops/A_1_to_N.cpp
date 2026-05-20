7efbe59d23e260a72e55aea52b90ac5eee0f9ed8
mohammedmegahed2010-del <mohammedmegahed2010@gmail.com>
2026-05-20 22:58:39 +0300

    Add files via upload

--- 2_Loops/A_1_to_N.cpp
+++ 2_Loops/A_1_to_N.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+// Prblem_Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432
+
+int main(){
+    short int N;
+    cin >> N;
+    for (short int i = 1; i <= N; i++){
+        cout << i << "\n";
+    }
+    return 0;
+}
